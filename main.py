ee2d4f84d893f94213f45ccc6f6df18a31fbc2d2
HSSCZ <HSSCZ@users.noreply.github.com>
2016-08-22 01:27:12 -0600

    Change to python3 & minor fixes

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-from __future__ import print_function
+#!/usr/bin/env python3
 import requests
 
 currencies = ['aud', 'cad', 'chf', 'cnh', 'cny', 'eur', 'gbp', 'hkd', 'ils',
@@ -9,22 +8,17 @@ def main(args):
     print('%.2f %s' % (convert(args.value, args.fc, args.tc), args.tc))
 
 def convert(value, fromcur, tocur):
-    url = 'http://download.finance.yahoo.com/d/quotes.csv?s=%s%s=X&f=l1'\
-          % (fromcur, tocur)
+    url = 'http://download.finance.yahoo.com/d/quotes.csv?s={}{}=X&f=l1'.format(fromcur, tocur)
     query = requests.get(url)
     rate = float(query.text)
     return value * rate
 
 if __name__ == '__main__':
     import argparse
-    parser = argparse.ArgumentParser(prog='concur',
-             description='Accepted currencies: %s'%','.join(currencies))
+    parser = argparse.ArgumentParser(prog='concur', description='Accepted currencies: %s'%','.join(currencies))
     parser.add_argument('value', help='Value', type=float)
-    parser.add_argument('fc', help='Currency to convert from.',
-                        choices=currencies, metavar='fc')
-    parser.add_argument('tc', help='Currency to convert to.  Default: USD',
-                        choices=currencies, default='usd', nargs='?',
-                        metavar='tc')
+    parser.add_argument('fc', help='Currency to convert from.', choices=currencies)
+    parser.add_argument('tc', help='Currency to convert to. Default: USD', choices=currencies, default='usd')
     args = parser.parse_args()
     args.fc = args.fc.upper()
     args.tc = args.tc.upper()
