29efaf2069cb1ecfd36405cb7a93828479b06438
HSSCZ <HSSCZ@users.noreply.github.com>
2016-08-22 02:46:03 -0600

    change filename and update string formatting

--- concur.py
+++ concur.py
@@ -5,7 +5,7 @@ currencies = ['aud', 'cad', 'chf', 'cnh', 'cny', 'eur', 'gbp', 'hkd', 'ils',
               'inr', 'jpy', 'krw', 'mxn', 'nzd', 'rub', 'sek', 'sgd', 'usd']
 
 def main(args):
-    print('%.2f %s' % (convert(args.value, args.fc, args.tc), args.tc))
+    print('{:.2f} {}'.format(convert(args.value, args.fc, args.tc), args.tc))
 
 def convert(value, fromcur, tocur):
     url = 'http://download.finance.yahoo.com/d/quotes.csv?s={}{}=X&f=l1'.format(fromcur, tocur)
@@ -15,7 +15,7 @@ def convert(value, fromcur, tocur):
 
 if __name__ == '__main__':
     import argparse
-    parser = argparse.ArgumentParser(prog='concur', description='Accepted currencies: %s'%','.join(currencies))
+    parser = argparse.ArgumentParser(prog='concur', description='Accepted currencies: {}'.format(', '.join(currencies)))
     parser.add_argument('value', help='Value', type=float)
     parser.add_argument('fc', help='Currency to convert from.', choices=currencies)
     parser.add_argument('tc', help='Currency to convert to. Default: USD', choices=currencies, default='usd', nargs='?')
